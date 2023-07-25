088930fc8d57c3cdf590977d351ee0a33842bdd1
kawalaseania <=>
2023-07-24 18:45:07 -0700

    Write a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - accept a string wihch will be reversed
+ * @s: is the string variable
+ * Return: the length of the scting
+ */
+
+int _strlen(char *s)
+{
+	int string_length;
+
+	string_length = strlen(s);
+	return (string_length);
+}
